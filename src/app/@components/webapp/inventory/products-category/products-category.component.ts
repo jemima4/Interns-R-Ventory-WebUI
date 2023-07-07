1af7a8855a48d1959562f23d64f6a659cdb21836
Kolawole <omotoshok9@gmail.com>
2023-07-07 03:47:37 +0100

    Table bug still on

--- src/app/@components/webapp/inventory/products-category/products-category.component.ts
+++ src/app/@components/webapp/inventory/products-category/products-category.component.ts
@@ -1,10 +1,134 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { AddNewCategoryComponent } from './add-new-category/add-new-category.component';
+import { Subscription } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+
+export interface Category {
+  check: true;
+  category: string;
+  createdBy: string;
+  date: Date;
+  lastToUpdate: string;
+  updated: Date;
+  status: boolean;
+  action: boolean;
+}
 
 @Component({
   selector: 'app-products-category',
   templateUrl: './products-category.component.html',
-  styleUrls: ['./products-category.component.scss']
+  styleUrls: ['./products-category.component.scss'],
 })
-export class ProductsCategoryComponent {
+export class ProductsCategoryComponent implements OnInit, OnDestroy {
+  subscription = new Subscription();
+  constructor(public dialog: MatDialog) {}
 
+  // Table data
+  displayedColumns: string[] = [
+    'select',
+    'Categorie',
+    'Created By',
+    'Created Date',
+    'Last Updated By',
+    'status',
+    'Action',
+  ];
+  datasource: MatTableDataSource<any[]> | null = null;
+
+  dummyData: any[] = [
+    {
+      check: true,
+      category: 'Casual Shoes',
+      createdBy: 'Kola',
+      date: new Date(),
+      lastToUpdate: 'Fatima',
+      updated: new Date(),
+      status: true,
+      action: false,
+    },
+    {
+      check: true,
+      category: 'Casual Shoes',
+      createdBy: 'Kola',
+      date: new Date(),
+      lastToUpdate: 'Fatima',
+      updated: new Date(),
+      status: true,
+      action: false,
+    },
+    {
+      check: true,
+      category: 'Casual Shoes',
+      createdBy: 'Kola',
+      date: new Date(),
+      lastToUpdate: 'Fatima',
+      updated: new Date(),
+      status: true,
+      action: false,
+    },
+    {
+      check: true,
+      category: 'Casual Shoes',
+      createdBy: 'Kola',
+      date: new Date(),
+      lastToUpdate: 'Fatima',
+      updated: new Date(),
+      status: true,
+      action: false,
+    },
+    {
+      check: true,
+      category: 'Casual Shoes',
+      createdBy: 'Kola',
+      date: new Date(),
+      lastToUpdate: 'Fatima',
+      updated: new Date(),
+      status: true,
+      action: false,
+    },
+    {
+      check: true,
+      category: 'Casual Shoes',
+      createdBy: 'Kola',
+      date: new Date(),
+      lastToUpdate: 'Fatima',
+      updated: new Date(),
+      status: true,
+      action: false,
+    },
+  ];
+
+  ngOnInit(): void {
+    this.datasource = new MatTableDataSource(this.dummyData);
+  }
+
+  openForm() {
+    const popUp = this.dialog.open(AddNewCategoryComponent, {
+      width: '26.4375rem',
+      height: '19.8125rem',
+      backdropClass: 'zns-dialog-backdrop',
+      autoFocus: true,
+      panelClass: 'dialog-test',
+
+      data: {
+        name: 'Omotosho Kolawole',
+        type: 'Angular',
+      },
+    });
+
+    this.subscription.add(
+      popUp.afterClosed().subscribe((data) => {
+        if (data) {
+          console.log(data.categoryName);
+        } else {
+          console.log('No dat passed');
+        }
+      })
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
 }
--- src/app/@components/webapp/inventory/products-category/products-category.module.ts
+++ src/app/@components/webapp/inventory/products-category/products-category.module.ts
@@ -4,10 +4,31 @@ import { ProductsCategoryComponent } from './products-category.component';
 import { HeaderModule } from '../../header/header.module';
 import { RouterModule } from '@angular/router';
 import { ProductCategoryRoutingModule } from './products-category-routing.module';
+import { AddNewCategoryComponent } from './add-new-category/add-new-category.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
-  declarations: [ProductsCategoryComponent],
-  imports: [CommonModule, HeaderModule, ProductCategoryRoutingModule],
+  declarations: [ProductsCategoryComponent, AddNewCategoryComponent],
+  imports: [
+    CommonModule,
+    HeaderModule,
+    ProductCategoryRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatRippleModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatTableModule,
+  ],
   exports: [ProductsCategoryComponent],
+  entryComponents: [AddNewCategoryComponent],
 })
 export class ProductsCategoryModule {}
